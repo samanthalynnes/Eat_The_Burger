1b12dc41ed1cea0a38ffecd61a709bd0131daaab
Samantha Lynnes <samanthalynnes522@gmailcom>
2019-12-15 18:29:01 -0600

    testing jawsdb for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,34 +1,29 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection(process.env.JAWSDB_URL);
+const connection;
 
-connection.connect();
+if (process.env.JAWSDB_URL) {
 
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-    if (err) throw err;
-
-    console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
-
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "password",
-//     database: "burgers_db"
-// });
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        // port: 3306,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+};
 
 // Make connection.
-// connection.connect(function (err) {
-//     if (err) {
-//         console.error("error connecting: " + err.stack);
-//         return;
-//     }
-//     console.log("connected as id " + connection.threadId);
-// });
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
 // Export connection for our ORM to use.
 module.exports = connection;
