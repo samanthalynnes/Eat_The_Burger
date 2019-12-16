f995c7778e406df0dc8ce090c1f518c22d84b733
Samantha Lynnes <samanthalynnes522@gmailcom>
2019-12-16 15:19:38 -0600

    working on jawsdb for heroku

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN,
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN DEFAULT FALSE,
 	PRIMARY KEY (id)
 );
