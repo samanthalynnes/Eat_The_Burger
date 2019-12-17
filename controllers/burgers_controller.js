73aca09302db7682f53258e96888d97fb986593b
Samantha Lynnes <samanthalynnes522@gmailcom>
2019-12-17 17:28:24 -0600

    working on jawsdb for heroku

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("*", function (req, res) {
+router.get("/", function (req, res) {
     burger.selectAll(function (data) {
         res.render("index", { burgers: data });
     });
